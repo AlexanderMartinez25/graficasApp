05ccf14b2b6f7951bb074c2dc5578bc67fa407bb
Alexander <alexander.martinez@lapatota.cl>
2022-07-21 22:56:09 -0400

    añadir flexibilidad a nuestro componente

--- src/app/graficas/components/grafica-barra/grafica-barra.component.ts
+++ src/app/graficas/components/grafica-barra/grafica-barra.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ChartOptions, ChartType, ChartDataSets } from 'chart.js';
 import { Label } from 'ng2-charts';
 
@@ -10,33 +10,31 @@ import { Label } from 'ng2-charts';
 })
 export class GraficaBarraComponent implements OnInit {
 
+  @Input() horizontal: boolean = false;
+  @Input() barChartLabels: Label[] = [
+    // '2020', '2021', '2022', '2023', '2024', '2025', '2026'
+  ];
+  @Input() barChartData: ChartDataSets[] = [
+    // { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A', backgroundColor: 'blue', hoverBackgroundColor: 'red' },
+    // { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' },
+    // { data: [2, 8, 70, 19, 26, 47, 50], label: 'Series C' }
+  ];
+
   public barChartOptions: ChartOptions = {
     responsive: true,
-    // We use these empty structures as placeholders for dynamic theming.
-    scales: {
-      xAxes: [{}],
-      yAxes: [{}]
-    },
-    plugins: {
-      datalabels: {
-        anchor: 'end',
-        align: 'end'
-      }
-    }
   };
-  public barChartLabels: Label[] = ['2020', '2021', '2022', '2023', '2024', '2025', '2026'];
   public barChartType: ChartType = 'bar';
   public barChartLegend = true;
 
-  public barChartData: ChartDataSets[] = [
-    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A', backgroundColor: 'blue', hoverBackgroundColor: 'red' },
-    { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' },
-    { data: [2, 8, 70, 19, 26, 47, 50], label: 'Series C' }
-  ];
 
-  constructor() { }
+  constructor() {
+
+  }
 
   ngOnInit(): void {
+    if (this.horizontal) {
+      this.barChartType = 'horizontalBar';
+    }
   }
 
 }
