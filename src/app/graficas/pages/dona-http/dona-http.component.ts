c2a90c43626d7ab919279bab32d87a4727b491ea
Alexander <alexander.martinez@lapatota.cl>
2022-07-21 23:25:31 -0400

    Mostrar la información de la gráfica

--- src/app/graficas/pages/dona-http/dona-http.component.ts
+++ src/app/graficas/pages/dona-http/dona-http.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { GraficasService } from '../../services/graficas.service';
+import { Label, MultiDataSet, Color } from 'ng2-charts';
+import { ChartType } from 'chart.js';
 
 @Component({
   selector: 'app-dona-http',
@@ -9,16 +11,38 @@ import { GraficasService } from '../../services/graficas.service';
 })
 export class DonaHttpComponent implements OnInit {
 
+  // Doughnut
+  public doughnutChartLabels: Label[] = [
+    // 'Download Sales', 'In-Store Sales', 'Mail-Order Sales', 'Other'
+  ];
+  public doughnutChartData: MultiDataSet = [
+    // [350, 450, 100, 22]
+  ];
+  public doughnutChartType: ChartType = 'doughnut';
+
+  public colors: Color[] = [
+    {
+      backgroundColor: [
+        '#0F60FD',
+        '#0E8EE3',
+        '#1CDEFA',
+        '#0EE3C5',
+        '#0FFD98'
+      ]
+    }
+  ]
   constructor(private gs: GraficasService) { }
 
   ngOnInit(): void {
-
     this.gs.getUsuariosRRSS()
       .subscribe(data => {
-        console.log(data);
+        const labels = Object.keys(data);
+        const values = Object.values(data);
+
+        this.doughnutChartLabels = labels;
+        this.doughnutChartData.push(values)
 
       })
-
   }
 
 }
