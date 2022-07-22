5a0fab5c7221e55c4f70515dba82258a3b8c175a
Alexander <alexander.martinez@lapatota.cl>
2022-07-21 23:14:42 -0400

    gráfica dona mediante petición http

--- src/app/graficas/pages/dona-http/dona-http.component.ts
+++ src/app/graficas/pages/dona-http/dona-http.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GraficasService } from '../../services/graficas.service';
 
 @Component({
   selector: 'app-dona-http',
@@ -8,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DonaHttpComponent implements OnInit {
 
-  constructor() { }
+  constructor(private gs: GraficasService) { }
 
   ngOnInit(): void {
+
+    this.gs.getUsuariosRRSS()
+      .subscribe(data => {
+        console.log(data);
+
+      })
+
   }
 
 }
